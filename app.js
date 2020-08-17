e7f35aa4d4c7ffdecaac25a78a96b4bb18b81395
raphaelanjos1 <raphael-anjos@live.com>
2020-08-16 23:23:59 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://roa-node-express-jwt-auth:7cYugP9bpspNRA8D@node-express-jwt-auth.ppvkn.gcp.mongodb.net/node-express-jwt-auth-db";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
